3d2d6d11a1288efbf6257384e7bef89264074aee
The Phantom Technology -Advanced Pentest <155259033+The-Phantom-Technology@users.noreply.github.com>
2024-12-01 19:26:57 +0800

    更新

---
+++ PHOSBUILD.sh
@@ -0,0 +1,24 @@
+rm -rf /opt/PHANTX/thc-ipv6
+
+cd /opt/PHANTX
+
+git clone https://github.com/vanhauser-thc/thc-ipv6
+
+cd /opt/PHANTX/thc-hydra
+
+
+if [ $? -eq 0 ]
+then
+  # Result is OK! Just continue...
+  echo "Copy PACKAGE... PASS!"
+else
+  # houston we have a problem
+  exit 1
+fi
+
+make
+make install
+
+chmod -R 755 /opt/PHANTX
+rm -rf /opt/PHOS/tool/thc-ipv6
+rm -rf /opt/PHOS/tool/thc-ipv6
